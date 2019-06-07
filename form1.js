fe5e7b3741561354f5185f092ff20849ef90639c
harshada <harshadawani06@gmail.com>
2019-06-08 00:40:55 +0530

    first commit

---
+++ form1.js
@@ -0,0 +1,15 @@
+var myapp = new Vue({
+    el:"#my-app",
+    data:{
+      name:"Online Web tutor",
+      title:"This is channel link",
+      imageLink:"gallery1.jpg",
+      isActive:true,
+      ulink:"C:\Users\harshada\Desktop/gallary1.jpg",
+      txtName:"",
+      txtEmail:"",
+      txtmale:"",
+      txtfemale:"",
+      htmlContent:"<p>This is simple form example</p>"
+    }
+});  
